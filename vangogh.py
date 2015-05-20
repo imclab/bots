5c831d2610d2058647047c9b5fd58c0a5fad63d7
Alex J. Champandard <alexjc@aigamedev.com>
2015-05-20 18:59:03 +0200

    Adding explicit license, removing some unused code and warnings.

--- vangogh.py
+++ vangogh.py
@@ -1,15 +1,30 @@
+"""
+Copyright (C) 2015, Alex J. Champandard.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+"""
+ 
 import random
 
 import numpy
 import scipy.misc
 import skimage.transform
 
+
 SCALE = 8
 SAMPLES = 50000
 
 img_high = scipy.misc.imread('VG_DT1567.jpg')
-res_low = (img_high.shape[0]/8, img_high.shape[1]/8) 
 img_low = skimage.transform.downscale_local_mean(img_high, (SCALE,SCALE,1))
+img_plain = scipy.misc.imresize(img_low, img_high.shape)
 
 
 a_in = numpy.zeros((SAMPLES, 8 * 8 * 3), dtype=numpy.float32)
@@ -37,7 +52,7 @@ print a_in.min(), a_in.max(), a_out.min(), a_out.max()
 
 nn = Regressor(
         layers=[
-            Layer("Rectifier", units=1024, pieces=2),
+            Layer("Rectifier", units=1024),
             Layer("Linear"),
         ],
         learning_rate=0.001,
@@ -64,4 +79,5 @@ for i, (yh, xh) in enumerate(samples):
     img_test[yh-8:yh+8,xh-8:xh+8] = (a_test[i].reshape(16,16,3) + 0.5) * 255.0
 
 print "Reconstructing..."
-scipy.misc.imsave('VG_DT1567.test.png', img_test)
+# scipy.misc.imsave('VG_DT1567.test.png', img_test)
+scipy.misc.imsave('VG_DT1567.plain.png', img_plain)
